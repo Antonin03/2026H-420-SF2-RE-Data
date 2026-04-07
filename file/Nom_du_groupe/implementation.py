5e9a7d4f55e51d146f0026a2dc418643310cd765
Rémi Fortier (SuperMamimou) <fortierremi08@gmail.com>
2026-04-07 08:54:02 -0400

    Explication du fonctionnement dans implementations ajout de la methode close pour enregister les changements Retirer Herobrine

--- file/Nom_du_groupe/implementation.py
+++ file/Nom_du_groupe/implementation.py
@@ -1,5 +1,3 @@
-#Les affaires avec les fichier vont chercher une liste qui serait dans l'autre document
-
 import json
 
 class File:
@@ -7,36 +5,54 @@ class File:
         self.base_de_donnee = []
         self.index_tete = 0
         self.filename = filename
-        self.load_file()
-        
+        self.load_file() #Pourquoi load_file est dans le constructeur?
+                         #En mettant la fonction load_file dans le constructeur
+                         #L'utilisateur n'a pas besoin de l'appeler pour que les données soient importées
     def load_file(self):
+        """Méthode qui va rechercher dans un fichier une certaine base de donnée. \n
+        Ne retourne rien, mais modifie les attributs self.base_de_donnee et self.index_tete. \n
+        self.base_de_donnee --> La base de donnée obtenue dans le fichier. \n
+        self.index_tete  --> L'index du premier "vrai" élément"""
         with open(self.filename, "r") as f:
             file = json.load(f)
         for i in file:
             self.base_de_donnee.append(i)
+        self.index_tete = self.base_de_donnee[0]
 
-    def est_vide(self):
-        if len(self.base_de_donnee) == 0:
+    def est_vide(self) -> bool:
+        """Methode qui evalue si une base de donnée File est "vide"."""
+        if self.base_de_donnee[self.index_tete]: #On regarde juste s'il y a un élément a l'index_tete, 
+            return False                         #Sinon c'est que la File est "vide"
+        else:
             return True
-        return False
-    
+            
     def enqueue(self, valeur):
+        """Méthode qui ajoute un élément à la fin de la File"""
         self.base_de_donnee.append(valeur)
 
     def dequeue(self):
+        """Fonction qui "retire" la tête de la File et la retourne."""
         if self.est_vide():
             return "Erreur: La file est vide"
         else:
-            tete = self.base_de_donnee[self.index_tete]
-            self.base_de_donnee[self.index_tete] = None
-            self.index_tete += 1
-            return tete
+            tete = self.base_de_donnee[self.index_tete]  #Que ce passe-t-il ici?
+            self.base_de_donnee[self.index_tete] = None  #En gros, on retire le dernier élément en:
+            self.index_tete += 1                         # le remplacant par None et en bougeant l'index 0 un peu plus loin.
+            self.base_de_donnee[0] += 1                  #Si la liste est très grande, c'est inefficace de retirer et plus
+            return tete                                  #efficace de remplacer.
     
     def size(self):
-        return len(self.base_de_donnee)
-
+        """Méthode qui retourne la taille d'une file."""
+        return len(self.base_de_donnee[self.index_tete :]) #Il ne faut pas oublier qu'on retourne 
+                                                           #la taille de la File qu'on utilise.
     def front(self):
+        """Méthode qui retourne le premier élément de la liste sans le retirer."""
         if self.est_vide():
             return "Erreur: La file est vide"
         else:
-            return self.base_de_donnee[0]
+            return self.base_de_donnee[self.index_tete]
+    
+    def close(self):
+        """Méthode qui renvoie les modifications qu'on a fait aux données dans le fichier originale."""
+        with open(self.filename, "w") as f:
+            json.dump(self.base_de_donnee, f, indent= 2)
